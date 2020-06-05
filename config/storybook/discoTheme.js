4b5b12e0d7ad4d5694b817a6eb1b0f69e95b2753
fernandogelin <fernando@fernandogelin.com>
2020-06-05 18:11:12 -0400

    feat: 🎸 add storybook level theme

---
+++ config/storybook/discoTheme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create';
+import logo from './public/disco.svg';
+
+export default create({
+  base: 'light',
+
+  brandTitle: 'Disco Design System',
+  brandUrl: 'https://discodesign.ccv.brown.edu',
+  brandImage: logo
+});
