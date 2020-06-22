4e692bcfe64ba9cbc7d4017614e0947810adba8b
Rashi Dhar <rdhar1997@gmail.com>
2020-06-22 14:09:56 -0400

    feat: modal vue component

---
+++ src/components/d-modal.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="modal">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">{{ title }}</p>
+        <button class="delete" aria-label="close"></button>
+      </header>
+      <section class="modal-card-body">
+        <slot name="content"></slot>
+      </section>
+      <footer class="modal-card-foot">
+        <slot name="footer"></slot>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
