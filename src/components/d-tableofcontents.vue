eb86a6bc64a80ede17f4995af4423314a784bcd2
mauradriscoll <51652231+mauradriscoll@users.noreply.github.com>
2020-07-27 17:34:09 -0400

    feat: 🎸 First attempt at table of contents
    Added story to check progress, icons and names show up with numbers

---
+++ src/components/d-tableofcontents.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="d-toc">
+    <table :name="name" class="d-toc">
+      <tbody>
+        <tr v-for="(entry, index) in data" :key="index">
+          <td>
+            <d-icon :name="entry.icon"></d-icon>
+          </td>
+          <td>
+            <h1>{{ entry['name'] }}</h1>
+          </td>
+          <td>
+            <h1>{{ index + 1 }}</h1>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import DIcon from '@/components/d-icon.vue';
+console.log('here');
+export default {
+  components: {
+    'd-icon': DIcon
+  },
+  props: {
+    data: {
+      type: Array,
+      required: true
+    },
+    name: {
+      type: String
+    }
+  },
+  computed: {
+    textColor() {
+      let color =
+        ['danger', 'dark', 'link'].indexOf(this.variant) !== -1
+          ? 'has-text-light'
+          : 'has-text-dark';
+      if (this.outlined) {
+        color = `has-text-${this.variant}`;
+      }
+      return color;
+    }
+  },
+  methods: {
+    onClick() {
+      this.$emit('click');
+    }
+  }
+};
+</script>
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as DButton } from './d-button.vue';
 export { default as DHero } from './d-hero.vue';
 export { default as DIcon } from './d-icon.vue';
+export { default as DTOC } from './d-tableofcontents.vue';
 export { default as CCVLogo } from './logos/CCVLogo.vue';
 export { default as BrownLogo } from './logos/BrownLogo.vue';
