41656244c1e90b96e3269e635c9a59aa389cbae2
mauradriscoll <51652231+mauradriscoll@users.noreply.github.com>
2020-07-28 14:18:01 -0400

    feat: 🎸 Updated formatting and hover for toc

--- src/components/d-tableofcontents.vue
+++ src/components/d-tableofcontents.vue
@@ -1,20 +1,24 @@
 <template>
   <div class="d-toc">
-    <table :name="name" class="d-toc">
-      <tbody>
-        <tr v-for="(entry, index) in data" :key="index">
-          <td>
-            <d-icon :name="entry.icon"></d-icon>
-          </td>
-          <td>
-            <h1>{{ entry['name'] }}</h1>
-          </td>
-          <td>
-            <h1>{{ index + 1 }}</h1>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="box outer">
+      <table :name="name" class="d-toc">
+        <tbody>
+          <tr v-for="(entry, index) in data" :key="index">
+            <div class="box component">
+              <td class="component-icon">
+                <d-icon :name="entry.icon"></d-icon>
+              </td>
+              <td class="component-title fs-20">
+                <strong>{{ entry['name'] }}</strong>
+              </td>
+              <td class="component-number fs-20" align="right">
+                <h1>{{ index + 1 }}</h1>
+              </td>
+            </div>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -33,23 +37,66 @@ export default {
     name: {
       type: String
     }
-  },
-  computed: {
-    textColor() {
-      let color =
-        ['danger', 'dark', 'link'].indexOf(this.variant) !== -1
-          ? 'has-text-light'
-          : 'has-text-dark';
-      if (this.outlined) {
-        color = `has-text-${this.variant}`;
-      }
-      return color;
-    }
-  },
-  methods: {
-    onClick() {
-      this.$emit('click');
-    }
   }
+  // computed: {
+  //   textColor() {
+  //     let color =
+  //       ['danger', 'dark', 'link'].indexOf(this.variant) !== -1
+  //         ? 'has-text-light'
+  //         : 'has-text-dark';
+  //     if (this.outlined) {
+  //       color = `has-text-${this.variant}`;
+  //     }
+  //     return color;
+  //   }
+  // },
+  // methods: {
+  //   onClick() {
+  //     this.$emit('click');
+  //   }
+  // }
 };
 </script>
+
+<style lang="scss" scoped>
+.box {
+  background-color: white !important;
+  //width: 50%;
+  display: inline-block;
+}
+.component {
+  background-color: white !important;
+  width: 100%;
+  //box-shadow: initial;
+}
+
+div.box .component:hover {
+  background-color: grey !important;
+}
+
+.component-title {
+  width: 100%;
+}
+
+.component-number {
+  text-align: right;
+  color: grey;
+}
+
+.component-icon {
+  padding-right: 10px;
+}
+
+.outer {
+  flex: 0 0 50%;
+  max-width: 100%;
+  min-width: 50%;
+}
+.fs-20 {
+  font-size: 1.25rem !important;
+  line-height: 1.4em;
+}
+table {
+  width: 100%;
+}
+</style>
