0173968f8d39a2b41a8b585968649c758cc8feac
fernandogelin <fernando@fernandogelin.com>
2020-08-22 17:32:07 -0400

    refactor: refactor toc

--- src/components/d-table-of-contents.vue
+++ src/components/d-table-of-contents.vue
@@ -1,26 +1,16 @@
 <template>
-  <div class="d-toc">
-    <div class="box outer">
-      <table :name="name" class="d-toc">
-        <tbody>
-          <tr v-for="(entry, index) in data" :key="index">
-            <a :href="entry.link">
-              <div class="box component">
-                <td class="component-icon">
-                  <d-icon :name="entry.icon"></d-icon>
-                </td>
-                <td class="component-title fs-20">
-                  <strong>{{ entry['name'] }}</strong>
-                </td>
-                <td class="component-number fs-20" align="right">
-                  <h1>{{ index + 1 }}</h1>
-                </td>
-              </div>
-            </a>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+  <div class="d-toc-container">
+    <ul :name="name" class="d-toc">
+      <li v-for="(entry, index) in data" :key="index">
+        <a :href="entry.link" class="toc-content">
+          <d-icon class="toc-icon" :name="entry.icon"></d-icon>
+          <span class="toc-start fs-20">
+            {{ entry['name'] }}
+          </span>
+          <span class="toc-end fs-20">{{ index + 1 }}</span>
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -43,43 +33,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.box {
-  background-color: white !important;
-  display: inline-block;
+@import 'bulma';
+.d-toc-container {
+  display: grid;
+  justify-items: center;
 }
-.component {
-  background-color: white !important;
-  width: 100%;
-  //box-shadow: initial;
+.d-toc {
+  width: 60vw;
 }
-
-div.box .component:hover {
-  background-color: grey !important;
+.toc-content {
+  display: grid;
+  grid-template-columns: 1fr 10fr 10fr 1fr;
+  grid-template-rows: auto;
+  grid-template-areas: 'icon start . end';
+  padding: $size-5;
+  &:hover {
+    background-color: $light;
+  }
 }
-
-.component-title {
-  width: 100%;
+.toc-start {
+  grid-area: start;
 }
-
-.component-number {
-  text-align: right;
-  color: grey;
+.toc-end {
+  grid-area: end;
 }
-
-.component-icon {
-  padding-right: 10px;
-}
-
-.outer {
-  flex: 0 0 50%;
-  max-width: 100%;
-  min-width: 50%;
-}
-.fs-20 {
-  font-size: 1.25rem !important;
-  line-height: 1.4em;
-}
-table {
-  width: 100%;
+.toc-icon {
+  grid-area: icon;
 }
 </style>
