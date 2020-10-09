6b9672d96c0201d70cf6fe5d47c592dd1d13ade2
fernandogelin <fernando@fernandogelin.com>
2020-10-09 10:53:40 -0400

    fix: 🐛 fix tests

--- tests/unit/d-icon.spec.js
+++ tests/unit/d-icon.spec.js
@@ -2,29 +2,29 @@ import { render } from '@testing-library/vue';
 import DIcon from '@/components/d-icon.vue';
 import '@testing-library/jest-dom';
 
+const name = 'instagram';
+const family = 'brown';
+
 test('has icon class', () => {
-  const name = 'instagram';
   const { getByRole } = render(DIcon, {
-    props: { name },
+    props: { name, family },
   });
   expect(getByRole('img', { hidden: true })).toHaveClass('icon');
 });
 
 test('renders default height and width', () => {
-  const name = 'instagram';
   const { getByRole } = render(DIcon, {
-    props: { name },
+    props: { name, family },
   });
   expect(getByRole('img', { hidden: true })).toHaveAttribute('width', '24');
   expect(getByRole('img', { hidden: true })).toHaveAttribute('height', '24');
 });
 
 test('renders height and width when props are passed', () => {
-  const name = 'instagram';
   const width = '50';
   const height = '50';
   const { getByRole } = render(DIcon, {
-    props: { name, width, height },
+    props: { name, family, width, height },
   });
   expect(getByRole('img', { hidden: true })).toHaveAttribute('width', '50');
   expect(getByRole('img', { hidden: true })).toHaveAttribute('height', '50');
